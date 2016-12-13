694fa565e35ea51db3069dda37804b23798d8fe8
Lazara Ramos <ramos.lazara@gmail.com>
2016-12-12 21:07:48 -0800

    The program sends emails!

--- copy-row.js
+++ copy-row.js
@@ -1,30 +1,99 @@
 // Property of Lazara Ramos
 
-
-/*
- * assumes source data in sheet named Needed
- * target sheet of move to named Acquired
- * test column with yes/no is col 4 or D
- */
-
-function onEdit(event) {
+function spreadSheetEdit(event) {
   var ss = SpreadsheetApp.getActiveSpreadsheet(); // the whole spreadsheet
   var s = event.source.getActiveSheet(); // the source sheet
   var r = event.source.getActiveRange(); // the area the event fired in
 
-  if(s.getName() == "Ingredient-Request" && r.getColumn() == 6 && r.getValue() == "yes") {
-    var row = r.getRow(); // the row where the edit event occurred
-    var numColumns = s.getLastColumn(); // Returns the position of the last column that has content.
-    var targetSheet = ss.getSheetByName("form-submitted");
-    var target = targetSheet.getRange(targetSheet.getLastRow() + 1, 1); // write to the first blank row, starting from far left column
-    s.getRange(row, 1, 1, numColumns).copyTo(target);
+  // entry complete
+  if(isEntryComplete(s, r)) {
+    processEvent(ss, s, r, "Entry Complete");
   }
 
-  if(s.getName() == "form-submitted" && r.getColumn() == 7 && r.getValue() == "yes") {
-    var row = r.getRow(); // the row where the edit event occurred
-    var numColumns = s.getLastColumn(); // Returns the position of the last column that has content.
-    var targetSheet = ss.getSheetByName("added-to-WT");
-    var target = targetSheet.getRange(targetSheet.getLastRow() + 1, 1); // write to the first blank row, starting from far left column
-    s.getRange(row, 1, 1, numColumns).copyTo(target);
+  // approvedbyCulinary
+  if(approvedbyCulinary(s, r)) {
+    processEvent(ss, s, r, "Approved by Culinary");
   }
+
+  // rejectedbyCulinary - rejection email needed here
+  if(rejectedbyCulinary(s, r)) {
+    processEvent(ss, s, r, "Rejections");
+    sendEmail(s, r, 4, "culinary");
+  }
+
+  // approvedbyNutrition
+  if(approvedbyNutrition(s, r)) {
+    processEvent(ss, s, r, "Approved by Nutrition");
+  }
+
+  // rejectedbyNutrition - rejection email needed here
+  if(rejectedbyNutrition(s, r)) {
+    processEvent(ss, s, r, "Rejections");
+    sendEmail(s, r, 4, "nutrition");
+  }
+}
+
+function processEvent(spreadsheet, activeSheet, activeRange, sheetName) {
+  var row = activeRange.getRow();
+  var numColumns = activeSheet.getLastColumn();
+  var targetSheet = spreadsheet.getSheetByName(sheetName);
+  var target = targetSheet.getRange(targetSheet.getLastRow() + 1, 1);
+  activeSheet.getRange(row, 1, 1, numColumns).copyTo(target);
+}
+
+function isEntryComplete(activeSheet, activeRange) {
+  return activeSheet.getName() == "BRGR", "Vegetable Sides" && activeRange.getColumn() == 10 && activeRange.getValue() == "Entry Complete";
+}
+
+function approvedbyCulinary(activeSheet, activeRange) {
+  return activeSheet.getName() == "Entry Complete" && activeRange.getColumn() == 13 && activeRange.getValue() == "GB";
+}
+
+function rejectedbyCulinary(activeSheet, activeRange) {
+  return activeSheet.getName() == "Entry Complete" && activeRange.getColumn() == 13 && activeRange.getValue() == "reject";
+}
+
+function approvedbyNutrition(activeSheet, activeRange) {
+  return activeSheet.getName() == "Approved by Culinary" && activeRange.getColumn() == 15 && activeRange.getValue() == "KM" ;
+}
+
+function rejectedbyNutrition(activeSheet, activeRange) {
+  return activeSheet.getName() == "Approved by Culinary" && activeRange.getColumn() == 15 && activeRange.getValue() == "reject";
+}
+
+
+function sendEmail(activeSheet, activeRange, emailColIndex, rejectionType) {
+  var row = activeRange.getRow();
+  var numColumns = activeSheet.getLastColumn();
+
+  var emailCell = activeSheet.getRange(row, emailColIndex, 1, 1);
+  var emailAddress = emailCell.getDisplayValues()[0][0];
+
+  var mrfColIndex = 5;
+  var mrfCell = activeSheet.getRange(row, mrfColIndex, 1, 1);
+  var mrf = mrfCell.getDisplayValues()[0][0];
+
+  var recipeColIndex = 7;
+  var recipeCell = activeSheet.getRange(row, recipeColIndex, 1, 1);
+  var recipe = recipeCell.getDisplayValues()[0][0];
+
+  var emailBody = "MRF number: " + mrf + "\n" + "Recipe name: " + recipe + "\n";
+
+  if(rejectionType == "culinary") {
+    var culNotesColIndex = 12;
+    var culinaryNotesCell = activeSheet.getRange(row, culNotesColIndex, 1, 1);
+    var culinaryNotes = culinaryNotesCell.getDisplayValues()[0][0];
+    emailBody += "Culinary notes: " + culinaryNotes;
+  } else {
+    var nutNotesColIndex = 14;
+    var nutNotesCell = activeSheet.getRange(row, nutNotesColIndex, 1, 1);
+    var nutritionNotes = nutNotesCell.getDisplayValues()[0][0];
+    emailBody += "Nutrition notes: " + nutritionNotes;
+  }
+
+  MailApp.sendEmail(
+    emailAddress,
+    "Your recipe has been rejected",
+    emailBody
+  );
 }
